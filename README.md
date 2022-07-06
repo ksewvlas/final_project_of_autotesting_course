952ebc2c17045144070f22dd909507f3b1bf9d9a
Ksew <vlassovaksenia@gmail.com>
2022-07-06 18:44:33 +0600

    changed readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # final_project_of_autotesting_course
+
+1. `pip install -r requirements.txt`
+2. `pytest -v --tb=line --language=en -m need_review`
--- test_product_page.py
+++ test_product_page.py
@@ -25,7 +25,6 @@ class TestUserAddToBasketFromProductPage:
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-        # new_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
         page = ProductPage(browser, link)
         page.open()
         page.should_be_add_to_cart_button()
@@ -38,7 +37,6 @@ class TestUserAddToBasketFromProductPage:
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    # new_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart_button()
@@ -81,6 +79,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 @pytest.mark.need_review
